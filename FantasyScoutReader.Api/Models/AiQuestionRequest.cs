0a2cfaefe35eb526ceb1cdd796f1154fac6db7d2
ISRAEL JULIO <israel.jhonata@gmail.com>
2026-04-30 07:47:09 -0300

    modelos para pesquisa e resposta

---
+++ FantasyScoutReader.Api/Models/AiQuestionRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace FantasyScoutReader.Api.Models;
+
+public class AiQuestionRequest
+{
+    public string Question { get; set; } = string.Empty;
+}
---
+++ FantasyScoutReader.Api/Models/AiQuestionResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace FantasyScoutReader.Api.Models;
+
+public class AiQuestionResponse
+{
+    public string Answer { get; set; } = string.Empty;
+}
